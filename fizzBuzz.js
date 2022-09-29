dd7e26acc753f98991e82b674b70d7fe662a8ec3
wspenc <105823133+wspenc@users.noreply.github.com>
2022-09-29 10:26:15 -0600

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,14 +1,16 @@
 // Write your solution below this line:
 
-for (let i = 1; i < 50; i++) {
-    if (i % 3 ==  0){
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+    console.log("fizzbuz");
+    }else if (i % 3 ==  0){
         console.log("fizz");
     }else if (i % 5 == 0){
         console.log("buzz");
-    }else if (i % 3 == 0 && i % 5 == 0){
-        console.log("fizzbuz");
     }else{
         console.log(i);
     }
 }
+
+
  
